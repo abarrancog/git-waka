ff036265fcc711828b40e541c6e8fc936052c897
abarrancog <mekoahdd@yahoo.com>
2016-09-07 18:51:27 -0500

    all the code implement wakatime git

---
+++ mygit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+mygit() {
+
+  wakaapikey="#### Your API KEY #####"
+  
+  wakatime=$(date +%s)
+  
+  read -ra files <<< $(git ls-files -m)
+
+  for i in "${files[@]}"
+
+    do
+  
+    git diff $i
+
+    read -p "Message For File ($i): " message
+
+    git commit $i -m "$message"
+
+  done
+  
+  git pull origin master && git push origin master
+  
+  echo "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}"
+  curl -v -0 -i -X POST "https://wakatime.com/api/v1/users/current/heartbeats?api_key=$wakaapikey" \
+  -H "Content-Type: application/json" \
+  -H "Cache-Control: no-cache" \
+  -d "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}"
+}
+
+alias GC=mygit
