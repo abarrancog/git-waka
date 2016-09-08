aec34bf66c057f766bac3008d7442562e13d1286
abarrancog <mekoahdd@yahoo.com>
2016-09-08 18:23:08 -0500

    Coment and Remove verbose mode

--- mygit.sh
+++ mygit.sh
@@ -22,8 +22,8 @@ mygit() {
   
   git pull origin master && git push origin master
   
-  echo "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}"
-  curl -v -0 -i -X POST "https://wakatime.com/api/v1/users/current/heartbeats?api_key=$wakaapikey" \
+  #echo "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}"
+  curl -0 -i -X POST "https://wakatime.com/api/v1/users/current/heartbeats?api_key=$wakaapikey" \
   -H "Content-Type: application/json" \
   -H "Cache-Control: no-cache" \
   -d "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}"
