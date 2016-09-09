552dbb6487b2e6b66e4bb768b8410ce5b5022299
abarrancog <mekoahdd@yahoo.com>
2016-09-09 11:38:05 -0500

    add silent output and confirm response

--- mygit.sh
+++ mygit.sh
@@ -26,7 +26,8 @@ mygit() {
   curl -0 -i -X POST "https://wakatime.com/api/v1/users/current/heartbeats?api_key=$wakaapikey" \
   -H "Content-Type: application/json" \
   -H "Cache-Control: no-cache" \
-  -d "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}"
+  -d "{\"entity\": \"$(git rev-parse --show-toplevel)\",\"type\": \"app\", \"time\": $wakatime, \"project\": \"$(git rev-parse --show-toplevel  | sed 's#.*/##')\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\",  \"language\": \"GIT\",  \"lines\": 0}" \
+  -o /dev/null  --write-out '%{http_code} : Response WakaTime Ok is eq (201)\n'
 }
 
 alias GC=mygit
